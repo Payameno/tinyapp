99c241b3d8a448b05e60e8b44e52884f0713d066
Payam Beigi <beigi.payam@gmail.com>
2022-06-13 21:47:38 +0000

    hello URL added to check HTML strings both in browser and cmd

--- express_server.js
+++ express_server.js
@@ -11,6 +11,14 @@ app.get('/', (req, res) => {
   res.send("Hey there!");
 });
 
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+})
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>world</b></body><html>\n")
+})
+
 app.listen(port, () => {
   console.log(`I'm listening to you on ${port}`)
 });
