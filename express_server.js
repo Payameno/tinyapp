e6286e59885992b753cec8ec06913fd073275d3c
Payam Beigi <beigi.payam@gmail.com>
2022-06-14 05:51:21 +0000

    Receiving the post request, consol loging the body object -boy parser generated) and adding a a function to generate a 7-character string randomly - to be used as ShortURL key

--- express_server.js
+++ express_server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
 const port = 8080;
-
 const urlDatabase = {
-  "b3s2xk4": "http://www.facebook.com",
-  "l2ldwo1": "http://www.google.com"
+  "b3s2xk4a": "http://www.facebook.com",
+  "l2ldwo1r": "http://www.google.com"
 };
 
 app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({extended : true}));
 
 app.get('/', (req, res) => {
   res.send("Hey there!");
@@ -15,23 +16,37 @@ app.get('/', (req, res) => {
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-})
+});
 
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>world</b></body><html>\n")
-})
+});
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
-})
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok')
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL], };
   res.render('urls_Show', templateVars);
-})
+});
 
 app.listen(port, () => {
   console.log(`I'm listening to you on ${port}`)
 });
+
+function generateRandomString() {
+  const randNum = (Math.random() + 1 );
+  const randomString = randNum.toString(36).substring(5);
+};
